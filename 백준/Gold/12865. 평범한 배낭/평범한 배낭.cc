64c8304d489d1e80ea67175d9dbeeb35153f6d51
luiheid <63488084+luiheid@users.noreply.github.com>
2024-07-05 10:25:18 +0900

    Update 평범한 배낭.cc
    improved code

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,56 +1,36 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-struct Thing{
-  int w;
-  int v;
+struct item{
+    int weight;
+    int value;
 };
 
-bool compare(Thing a, Thing b){
-  if(a.w == b.w){
-    return a.v > b.v;
-  }
-  return a.w < b.w;
-}
-
 int main(){
+    int n, k;
 
-  int n, k;
-  cin >> n >> k;
+    cin >> n >> k;
 
-  Thing* things = new Thing[n];
-  for(int i=0; i<n; i++){
-    cin >> things[i].w >> things[i].v;
-  }
-
-  sort(things, things + n, compare);
-  int** weights = new int*[n+1];
-  for(int i=0; i<n+1; i++){
-    weights[i] = new int[k+1];
-  }
-  for(int i=0; i<n+1 ; i++){
-    for(int j=0; j<k+1 ; j++){
-      weights[i][j] = 0;
+    vector<struct item> items(n);
+    for(int i=0; i<n; i++){
+        cin >> items[i].weight >> items[i].value;
     }
-  }
 
-  for(int i=1; i<n+1; i++){
-    for(int j=1; j<k+1; j++){
-      if(j >= things[i-1].w){
-        if(weights[i-1][j] < weights[i-1][j-things[i-1].w] + things[i-1].v){
-          weights[i][j] = weights[i-1][j-things[i-1].w] + things[i-1].v;
+    vector<vector<int>> map(n+1, vector<int>(k+1, 0));
+
+    for(int i=1; i<n+1; i++){
+        for(int j=1; j<k+1; j++){
+            map[i][j] = map[i-1][j];
+            if(j >= items[i-1].weight){
+                map[i][j] = max(map[i][j], map[i-1][j-items[i-1].weight] + items[i-1].value);
+            }
         }
-        else{
-          weights[i][j] = weights[i-1][j];
-        }
-      }
-      else{
-        weights[i][j] = weights[i-1][j];
-      }
     }
-  }
 
-  cout << weights[n][k];
+    cout << map[n][k];
+
+    return 0;
 }
